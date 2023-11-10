a20d325c86ac6b24e1ca6263e8719f92d16cb109
0viktory0 <36894469+0viktory0@users.noreply.github.com>
2023-11-10 19:59:54 +0300

    fixed environment variables validation

--- pedantic2.py
+++ pedantic2.py
@@ -1,39 +1,24 @@
 # Написать вложенную схему валидации переменных окружения (настройки приложения), получить ошибки валидации
 
-from pydantic import BaseModel
+from pydantic import BaseSettings
 
 
-class DatabaseSettings(BaseModel):
+class Settings(BaseSettings):
     name: str
     password: str
-
-class DjangoSettings(BaseModel):
     DEBUG: bool
     SECRET_KEY: str
-    DATABASE: DatabaseSettings
     ALLOWED_HOSTS: list[str] = []
 
-
-data = {
-    "DEBUG": "True",
-    "SECRET_KEY": "mysecretkey",
-    "DB_NAME": "mydb",
-    "DB_PASSWORD": "mypassword",
-    "ALLOWED_HOSTS": "localhost,127.0.0.1",
-}
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
 
 
 try:
-    django_settings = DjangoSettings(
-        DEBUG=bool(data["DEBUG"]),
-        SECRET_KEY=data["SECRET_KEY"],
-        DATABASE=DatabaseSettings(
-            name=data["DB_NAME"],
-            password=data["DB_PASSWORD"],
-        ),
-        ALLOWED_HOSTS=data["ALLOWED_HOSTS"].split(",")
-    )
-    print("Переменные окружения валидны:", django_settings.json())
+    settings = Settings()
+
+    print("Переменные окружения валидны:", settings.json())
 except Exception as e:
     print("Произошла ошибка валидации переменных окружения:", e)
 
